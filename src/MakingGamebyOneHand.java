f5becb1465a395779043c9f5d5c8b3d6eb64329c
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-23 16:07:00 +0900

    수정

--- src/MakingGamebyOneHand.java
+++ src/MakingGamebyOneHand.java
@@ -1,16 +1,16 @@
 public class MakingGamebyOneHand {
     public static void main(String[] args) {
         String a = "1";
-        String b = "2";
-        String c = "3";
+        String b = "";
+        String c = "2";
     
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
 
+        b = c;
         c = a;
         a = b;
-        b = c;
 
         System.out.println(a);
         System.out.println(b);
