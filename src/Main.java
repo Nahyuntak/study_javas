6dee7273980ea281006cbff5a3ca9b110997c0c5
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-23 13:10:09 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Hyuntak NA";
+        System.out.println(name); // ; :문장의 종지부, "": 문자인식, println: 값 출력
+        // return 0;
+    }
+}
