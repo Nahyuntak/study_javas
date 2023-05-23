9ffcc559e9e16292e2244a65b32ac6eee5b22101
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-23 18:02:57 +0900

    수정

---
+++ src/SetWandGetVariable.java
@@ -0,0 +1,36 @@
+public class SetWandGetVariable {
+        public static void main(String[] args) {
+    
+            int a = 1;
+            int b = 3;
+            int c = 5;
+            int d = 7;
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+
+            if (b == 3) {
+                a = 10;
+            } 
+             if (d == 9 ) {
+                b = c;
+             } else {
+                b = 200;
+             }
+             if (a == 10) {
+                c = 50;
+             } else {
+                d = 30;
+             }
+            
+             System.out.println(a);
+             System.out.println(b);
+             System.out.println(c);
+             System.out.println(d);
+    
+            // return 0;
+        }
+    }
+
