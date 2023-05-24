959abf250943f072e718f50b15f8e5dfe1136118
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-24 10:54:57 +0900

    LoopWhile

---
+++ src/LoopWithCounter.java
@@ -0,0 +1,13 @@
+public class LoopWithCounter {
+    public static void main(String[] args) {
+        int a = 1;
+
+        while (a < 5) {
+            System.out.println(a);
+            a = a + 1;
+        }
+
+        System.out.println(a);
+        // return 0;
+    }
+}
