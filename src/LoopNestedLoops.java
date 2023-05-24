4de830767969a6ba3c9cff85ad8688d70925287e
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-24 16:34:50 +0900

    수정

--- src/LoopNestedLoops.java
+++ src/LoopNestedLoops.java
@@ -4,7 +4,7 @@ public class LoopNestedLoops {
 
         
 
-        // if와 break문 사용하지 않고
+        // Not using "if "and "break" statements 
         for (int first = 1; first < 5; first = first + 1) {
             System.out.println("for" + first);
             for (int second = 1; second <= first; second = second + 1) {
@@ -15,7 +15,7 @@ public class LoopNestedLoops {
         System.out.println("end");
 
 
-        // if와 break문 사용해서
+        // Using "if "and "break" statements 
         for (int first = 1; first < 5; first = first + 1) {
             System.out.println("for" + first);
             for (int second = 1; second < 5; second = second + 1) {
