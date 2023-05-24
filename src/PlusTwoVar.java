470ed2a355aea14c4d41e0292301100925039e52
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-24 13:04:36 +0900

    PlusTwoVar

---
+++ src/PlusTwoVar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PlusTwoVar {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+      int a = 0;
+      int b = 0;
+      a = myObj.nextInt(); // Read user input
+      b = myObj.nextInt(); // Read user input1
+
+      System.out.println(a+b);  // Output user input
+        // return 0;
+    }
+}
+
