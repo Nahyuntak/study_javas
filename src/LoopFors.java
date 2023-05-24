ef16459f55110a90bed3df3f270581c75d354173
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-24 15:18:02 +0900

    LoopWithMark

---
+++ src/LoopFors.java
@@ -0,0 +1,11 @@
+public class LoopFors {
+    public static void main(String[] args) {
+        int second = 10;
+         for (int first = 5; first < second; first = first + 2) {
+            // code block to be executed
+            System.out.println(first);
+          }
+          System.out.println();
+        //return 0;
+    }
+}
---
+++ src/LoopWithMark.java
@@ -0,0 +1,11 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i = i + 1) {
+            String line = "line" + i;
+           // code block to be executed
+           System.out.println(line);
+         }
+       //return 0;
+
+    }
+}
