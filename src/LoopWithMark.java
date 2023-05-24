5ddebf6d433a48f3995f62af6696bcb9aabac43b
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-24 15:22:27 +0900

    수정

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -1,11 +1,11 @@
 public class LoopWithMark {
     public static void main(String[] args) {
-        for (int i = 1; i < 6; i = i + 1) {
-            String line = "line" + i;
+        for (int i = 1; i < 5; i = i + 1) {
+            String line = "for" + i;
            // code block to be executed
            System.out.println(line);
          }
+         System.out.println("end");
        //return 0;
-
     }
 }
