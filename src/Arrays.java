d8f77a98b5597552115d2703a195f0081213200b
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-25 13:24:11 +0900

    오전 실습 내용

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,9 +1,19 @@
 public class Arrays {
     public static void main(String[] args) {
-       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        String[][] numbers = { { "10", "20", "30", "40" }, { "50", "60", "70" } }; // row와 row의 차이를 ","로 구분
+        for (int second = 0; second < numbers.length; second = second + 1) {
+            for (int third = 0; third < numbers[second].length; third = third + 1) {
+                System.out.print(numbers[second][third] + ",");
+            }
+            System.out.println();
+        }
 
-    for (int first = 0; first < 4; first = first + 1) {
-     System.out.println(cars[first]);
-       }
-    } 
+        // String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Kia"};
+        // System.out.println(cars.length);
+        // for (int first = 0; first < cars.length; first = first + 1) {
+        // System.out.print(cars[first] + ",");
+        // }
+        // System.out.println();
+        // return 0;
+    }
 }
