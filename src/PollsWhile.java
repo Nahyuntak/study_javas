e2d70cd9d681b77bf599ef9b03a8833eebdada5e
Nahyuntak <hyuntak@snu.ac.kr>
2023-05-26 13:03:05 +0900

    타이틀 수정

--- src/PollsWhile.java
+++ src/PollsWhile.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class A {
+public class PollsWhile {
     public static void main(String[] args) {
         try {
             Scanner myObj = new Scanner(System.in);
